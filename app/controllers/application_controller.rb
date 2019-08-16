058ebca6856fd04a3b377ea12871f8a7631cd900
Brian Simonton <bsimonton78@gmail.com>
2019-08-16 14:18:18 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,7 +19,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/signup" do
-    user = User.new(:username => params[:username], :password => params[:password])
+    
     
      if params[:username] == "" || params[:password] == ""
        
